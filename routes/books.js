c239fe5162dd559457a980f2435f47436e516ca7
Samriddhi98 <karmacharyasamriddhi@gmail.com>
2020-11-04 13:33:34 +0545

    db connected

--- routes/books.js
+++ routes/books.js
@@ -1,43 +1,69 @@
 var express = require('express');
 var router = express.Router();
 var books = require('../resources/books');
+let Books = require('../models/books');
 
-router.get('/add' , function(req , res, next) {
-    res.render ('addBooks' , {
+router.get('/add', function (req, res, next) {
+    res.render('addBooks', {
         title: 'Add book',
-    
+
     });
 
 });
 
-router.post('/save' , function(req, res) {
+router.post('/save', function (req, res) {
     // console.log('insave fucntion....... ' , req.body)
-    books.push({...req.body, _id: `00${books.length + 1}`});
-    res.redirect('/');
-});
-
-router.get('/remove/:index', function(req, res){
-    books.splice(req.params.index,1);
-    res.redirect('/');
-});
-
-router.get('/edit/:_id', function(req,res){
-   const book = books.find((book) => book._id === req.params._id);
-   //console.log('book to edit.......',book);
-   res.render('editBooks',{
-       title: 'Edit Book',
-       book
-   })
-})
-
-router.post('/edit/:_id', function(req,res){
-    const book = books.find((book) => book._id === req.params._id);
-        book.title = req.body.title
-        book.author = req.body.author
-        book.description = req.body.description
-        book.genre = req.body.genre
+    // books.push({...req.body, _id: `00${books.length + 1}`});
+    const book = new Books(req.body);
+    let promise = book.save();
+    promise.then(() => {
+        console.log('Book added');
         res.redirect('/');
-    
-   
-})
+    })
+
+});
+
+router.get('/remove/:id', function (req, res) {
+    Books.remove({ _id: req.params.id }, function () {
+        res.redirect('/');
+    })
+    // console.log(req.params._id);
+    // books.splice(req.params.index,1);
+    //  res.redirect('/');
+});
+
+router.get('/edit/:id', function (req, res) {
+    //    const book = books.find((book) => book._id === req.params._id);
+    //    //console.log('book to edit.......',book);
+    //    res.render('editBooks',{
+    //        title: 'Edit Book',
+    //        book
+    //    })
+
+    Books.findOne({ _id: req.params.id }, function (err, book) {
+        res.render('editBooks', {
+            title: 'Edit Book',
+            book: book
+        });
+    })
+});
+
+
+
+router.post('/edit/:_id', function (req, res) {
+    // const book = books.find((book) => book._id === req.params._id);
+    //     book.title = req.body.title
+    //     book.author = req.body.author
+    //     book.description = req.body.description
+    //     book.genre = req.body.genre
+    //     res.redirect('/');
+
+    Books.findOneAndUpdate({ _id: req.params._id }, { $set: req.body }, function (err, book) {
+        console.log(book);
+        res.redirect('/');
+
+    });
+});
+
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var books = require('../resources/books')
+var books = require('../resources/books');
+let  Books = require('../models/books')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BookAPP' , bookList:books});
+// router.get('/', function(req, res, next) {
+//   Books.find({}, function(err, books) {
+//     if (!err) {
+//       res.render('index', { title: 'Book App', bookList: books });
+//     } else {
+//       console.log('error', err);
+//     }
+//   })
+
+//   // res.render('index', { title: 'BookAPP' , bookList:books});
+// });
+
+router.get('/', async function(req,res,next){
+  let books = await Books.find();
+  res.render('index', { title: 'Book App', bookList: books });
 });
 
 router.get('/samriddhi', function(req, res, next) {
